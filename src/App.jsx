336bd9d0328524685327921d571037b8425e15f3
Sodikcp <paramesh.rah@gmail.com>
2024-01-10 14:58:41 +0700

    feat:complete Debug

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const fetchData = () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get("https://dummyjson.com/products");
-        setProducts(response.data.data);
+        setProducts(response.data.products);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -21,14 +21,18 @@ function App() {
     <div className="app-container">
       <h1>Product List</h1>
       <div className="product-list">
-        {products.map((product) => (
-          <div className="product-card" key={product.id}>
-            <h2>{product.title}</h2>
-            <p>Description: {product.description}</p>
-            <p>Price: ${product.price}</p>
-            <img src={product.picture} alt={product.title} />
-          </div>
-        ))}
+        {products.map((product) => {
+          return (
+            <>
+              <div className="product-card" key={product.id}>
+                <h2>{product.title}</h2>
+                <p>Description: {product.description}</p>
+                <p>Price: ${product.price}</p>
+                <img src={product.thumbnail} alt={product.title} />
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
